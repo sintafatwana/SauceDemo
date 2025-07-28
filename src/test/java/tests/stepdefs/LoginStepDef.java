683f0dd7852970e7ad4d17384790a2d31854300c
sintafatwana <sfatwana@gmail.com>
2025-07-28 09:04:39 +0700

    Update LoginStepDef.java

--- src/test/java/tests/stepdefs/LoginStepDef.java
+++ src/test/java/tests/stepdefs/LoginStepDef.java
@@ -10,17 +10,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class LoginStepDef {
-    WebDriverManager.chromedriver().setup();
-    WebDriver driver = new ChromeDriver();
+   WebDriver driver;
 
     @Before
     public void beforeTest(){
-        driver = WebDriverManager.chromedriver().create();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     public void afterTest(){
